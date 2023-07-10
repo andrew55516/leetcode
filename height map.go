84d79f7ba47c4650782de571b76c64f1250e1184
Andrey Aksenov <aksenovandrey4@gmail.com>
2023-07-10 14:16:28 +0300

    ...

--- height map.go
+++ height map.go
@@ -4,24 +4,27 @@ package main
 Дан массив n неотрицательных целых чисел, представляющий карту высот.
 Ширина каждой отметки равна 1. Вычислите, сколько уровней может быть заполнено водой после дождя.
 //
-//       #
-//   #---##-#
-//_#-##-######
-Input: height = [0,1,0,2,1,0,1,3,2,1,2,1] //0 1 _ 2 _ _ _ 3 2 _ 2 _
+//       #----#
+//   #---##-#-#
+//_#-##-#######
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1,3] // 0 _ 0 _ _ 0 1 _ _ 1 _ 1
 //
 Ответ: 6
 */
 
 func NonEmptyLevels(height []int) int {
-	stack := []int{height[0]}
+	counter := make(map[int]int, 0)
+	indMap := make(map[int]struct{}, 0)
 	ans := 0
 	for _, v := range height {
-		for stack[len(stack)-1] > v {
-			stack = stack[:len(stack)-1]
-			ans += 1
+		if _, ok := indMap[v]; ok && v > 0 {
+			for i := 0; i < v; i++ {
+				ans += counter[i]
+				counter[i] = 0
+			}
 		}
-		stack = append(stack, v)
+		counter[v]++
+		indMap[v] = struct{}{}
 	}
-
 	return ans
 }
--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import "fmt"
 type test []int
 
 func main() {
-	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 3}
 	fmt.Println(NonEmptyLevels(height))
 }
